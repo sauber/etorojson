9628bcc469832b9270d54f5281a2ff485e34cefc
Søren Døssing <netcom@sauber.net>
2020-11-12 15:35:06 +0100

    Run test against docker image

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # etorojson
 Scripts for loading json data from eToro
+
+Run `./docker-build.sh` to create docker image.
+
+Run `./test.sh` to verify outcome of each load script.
--- test.sh
+++ test.sh
@@ -4,31 +4,34 @@
 #
 set -e
 
+# Run docker image
+etorojson="docker run --rm etorojson"
+
 # Verify output from Discover People
 #
 trap "echo discover failed; exit 1" ERR
-buf=$(./discover "dailyddmin=-25&gainmin=0&hasavatar=true&maxmonthlyriskscoremax=6&maxmonthlyriskscoremin=1&pagesize=20&popularinvestor=true&sort=-copiers")
+buf=$($etorojson discover "dailyddmin=-25&gainmin=0&hasavatar=true&maxmonthlyriskscoremax=6&maxmonthlyriskscoremin=1&pagesize=20&popularinvestor=true&sort=-copiers")
 jq -e '.Items' <(printf $buf) >/dev/null
 echo discover OK
 
 # Verify output from portfolio
 #
 trap "echo portfolio failed; exit 1" ERR
-buf=$(./portfolio jeppekirkbonde 2988943)
+buf=$($etorojson portfolio jeppekirkbonde 2988943)
 jq -e '.AggregatedPositions' <(printf $buf) >/dev/null
 echo portfolio OK
 
 # Verify output from Stats
 #
 trap "echo stats failed; exit 1" ERR
-buf=$(./stats jeppekirkbonde 2988943)
+buf=$($etorojson stats jeppekirkbonde 2988943)
 jq -e '.Data' <(printf $buf) >/dev/null
 echo stats OK
 
 # Verify output from Chart
 #
 trap "echo charts failed; exit 1" ERR
-buf=$(./chart jeppekirkbonde 2988943)
+buf=$($etorojson chart jeppekirkbonde 2988943)
 jq -e '.simulation' <(printf $buf) >/dev/null
 echo chart OK
 
